a20e81edd7524141d465e03889f489c31895d8e8
shantanuGrow <shantanu@blitznow.in>
2024-06-07 15:07:45 +0530

    fixes own IHS interchange

--- generatePDFs.js
+++ generatePDFs.js
@@ -20,7 +20,7 @@ function generateStars(rating) {
     'How would you rate the team member on "Insisting on Highest Standards"* Processes, products, and services with new and recurring defects will fail. "Insisting on Highest Standards" ensures what we build stays. It means ensuring defects don\'t get sent down the line and those once fixed remain fixed. A rating of 1 means the team member builds things that are not up to standard and with defects. A rating of 3 means what the team member builds is defect free. A rating of 5 means the team member ensures things are defect-free not just in what he does but also in whatever the team is building.',
     'Highlight a few instances of why you gave the team member a particular rating on "Insisting on Highest Standards". The question is meant to make you think about why you gave someone a rating on the "Insisting on Highest Standards" value.',
     'How would you rate the team member on "Ownership" One of the key tenets to building a successful team is "Ownership". You\'ll never need to follow up on a task with an Owner. Team members are expected to own and drive initiatives tagged to them. Owners act on behalf of the entire company, beyond just their own team. "That’s not my job" is something they\'ll never say. A rating of 1 would mean the team member is a Renter, not an Owner, they\'ll try to move things away from them and think short term. A rating of 5 would mean the team member never shies away from any task, anything that\'s tagged to them gets done.',
-    'Highlight a few instances of why you gave the team member a particular rating on "Ownership". The question is meant to make you think about why you gave someone a rating on the "Ownership" value.',
+    'Highlight a few instances of why you gave the team member a particular rating on "Ownership".  The question is meant to make you think about why you gave someone a rating on the "Ownership" value.',
     "What should this person do differently?",
   ];
   for (const data of excelData) {
@@ -198,16 +198,16 @@ function generateStars(rating) {
           <td>Peer Rating</td>
           <td>${parseFloat(ratingsData2.map(res => res["CO_1"])).toFixed(2)}</td>
           <td>${parseFloat(ratingsData2.map(res => res["BA_1"])).toFixed(2)}</td>
-          <td>${parseFloat(ratingsData2.map(res => res["IHS_1"])).toFixed(2)}</td>
           <td>${parseFloat(ratingsData2.map(res => res["Own_1"])).toFixed(2)}</td>
+          <td>${parseFloat(ratingsData2.map(res => res["IHS_1"])).toFixed(2)}</td>
           <td>${parseFloat(ratingsData2.map(res => res["PR Total"])).toFixed(2)}</td>
           </tr>
           <tr>
           <td>STL Rating</td>
           <td>${parseFloat(ratingsData2.map(res => res["CO_2"])).toFixed(2)}</td>
           <td>${parseFloat(ratingsData2.map(res => res["BA_2"])).toFixed(2)}</td>
-          <td>${parseFloat(ratingsData2.map(res => res["IHS_2"])).toFixed(2)}</td>
           <td>${parseFloat(ratingsData2.map(res => res["Own_2"])).toFixed(2)}</td>
+          <td>${parseFloat(ratingsData2.map(res => res["IHS_2"])).toFixed(2)}</td>
           <td>${parseFloat(ratingsData2.map(res => res["STL Total"])).toFixed(2)}</td>
         </tr>
          
